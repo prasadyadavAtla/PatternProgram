fc0c3a852d4bd7aa62db03483ef6f6e4314b0cdf
venkataprasad <prasadbabu457@gmail.com>
2020-07-26 20:01:54 +0530

    Commit the pattern programs

---
+++ src/pattern/RightAngleLeftside.java
@@ -0,0 +1,23 @@
+package pattern;
+
+public class RightAngleLeftside {
+	
+	/*1
+	 *12
+	 *123
+	 *1234
+	 */
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		for(int i=1;i<=4;i++)
+		{
+			for (int j=1;j<=i;j++)
+			{
+			System.out.print(j+" ");
+			}
+			System.out.println();
+		}
+	}
+
+}
---
+++ src/pattern/RightAngleRever.java
@@ -0,0 +1,17 @@
+package pattern;
+
+public class RightAngleRever {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		for(int i=4;i>=1;i--)
+		{
+			for (int j=1;j<=i;j++)
+			{
+			System.out.print(" * ");
+			}
+			System.out.println();
+		}
+	}
+
+}
